4fa0d3d5637285acda5d18273c68e6790c5497c1
dzming <dzm_work@163.com>
2024-03-15 07:57:27 +0000

    添加部分结构体

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .get_local_ip_address import get_local_ip_address
 from .singleton import singleton
 from .temporary_change_dir import temporary_change_dir, class_temporary_change_dir
-from .data_structures import is_rectangular_matrix
+from .data_structures import is_rectangular_matrix, AutoFIFO, CircularQueue
--- data_structures.py
+++ data_structures.py
@@ -23,6 +23,59 @@ def is_rectangular_matrix(matrix: List[List[Union[int, float]]]) -> Tuple[bool, 
     else:
         return (True, (len(matrix), len(matrix[0])))
 
-# 示例使用
-print(is_rectangular_matrix([[1, 2, 3], [4, 5, 6]]))  # 应返回(True, (2, 3))
-print(is_rectangular_matrix([[1, 2], [4, 5, 6]]))  # 应返回(False, (-1, -1))
+from collections import deque
+
+class AutoFIFO:
+    def __init__(self, max_size=10):
+        self.queue = deque()
+        self.max_size = max_size
+
+    def push(self, item):
+        # 当队列达到最大长度时，自动从左端（即队列前端）弹出一个元素
+        if len(self.queue) >= self.max_size:
+            self.queue.popleft()  # FIFO的行为
+        self.queue.append(item)
+
+    def to_list(self) -> List:
+        # 将deque转换成list
+        return list(self.queue)
+
+class CircularQueue:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+
+    def isFull(self):
+        return self.size == self.capacity
+
+    def isEmpty(self):
+        return self.size == 0
+
+    def enqueue(self, item):
+        if self.isFull():
+            raise Exception("Queue is full")
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = item
+        self.size += 1
+
+    def dequeue(self):
+        if self.isEmpty():
+            raise Exception("Queue is empty")
+        item = self.queue[self.front]
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return item
+
+    def __getitem__(self, index):
+        if index >= self.size or index < 0:
+            raise IndexError("Index out of range")
+        return self.queue[(self.front + index) % self.capacity]
+
+    def getMiddleElement(self):
+        if self.isEmpty():
+            raise Exception("Queue is empty")
+        middle_index = self.size // 2
+        return self.__getitem__(middle_index)
